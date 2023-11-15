3d02229ea0478241d4d5b1c90a94271c6710f0d9
Marixa82 <121823496+Marixa82@users.noreply.github.com>
2023-11-15 13:48:12 +0000

    Deploying to gh-pages from @ Marixa82/goit-react-hw-08-phonebook@cc19377cbef1813bce75c5adeeccd08608395d44 🚀

---
+++ static/js/561.00f9f516.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[561],{6561:function(e,r,t){t.r(r);var n=t(6494),c=t(3329);r.default=function(){return(0,c.jsx)("main",{children:(0,c.jsx)(n.ZC,{children:(0,c.jsx)("h1",{children:"PHONEBOOK"})})})}}}]);
+//# sourceMappingURL=561.00f9f516.chunk.js.map
