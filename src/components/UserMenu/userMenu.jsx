31657ec12f6979bcfba232e1eb7dd13e30d045f2
Marixa82 <121823496+Marixa82@users.noreply.github.com>
2023-11-25 20:22:16 +0200

    update um

--- src/components/UserMenu/userMenu.jsx
+++ src/components/UserMenu/userMenu.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { authOperations, authSelectors } from 'redux/auth';
+import { authOperations, authSelectors } from '../../redux/auth';
 import {  DefaultImg, Container, Span, Button } from "../StylesAppBar.styled";
 
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
-  const {name} = useSelector(authSelectors.selectUserName);
+  const name = useSelector(authSelectors.selectUserName);
   // const handleLogOut = () => {
   //       dispatch(authOperations.logOut());
   //   };
   return (
     <Container>
         <picture><DefaultImg/></picture>
-        <Span>Hello, {name}</Span>
+        <Span>Hello,{name}</Span>
         <Button onClick={() => dispatch(authOperations.logOut())}>Log Out</Button>
        
       
